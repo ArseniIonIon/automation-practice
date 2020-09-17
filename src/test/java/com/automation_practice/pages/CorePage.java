29f5e3a6f3fb012fc393b5b58bb4adc444e0c1a6
dbodac <dumitru.bodac@endava.com>
2020-09-17 15:09:32 +0300

    adding screenshot feature for every After Step

--- src/test/java/com/automation_practice/pages/CorePage.java
+++ src/test/java/com/automation_practice/pages/CorePage.java
@@ -6,10 +6,20 @@ import org.openqa.selenium.support.FindBy;
 
 public abstract class CorePage extends AbsPageFactory{
 
-    @FindBy(className = "sf-with-ul")
+    @FindBy(xpath = "//a[contains(@title,'Women') and contains(@class, 'sf-with-ul')]")
     protected WebElement womenOptionMenuBar;
+    @FindBy(xpath = "//a[contains(@class, 'sf-with-ul')]/../ul/li[1]/a[contains(@title,'T-shirts')]")
+    protected  WebElement tShirtWomenSubMenu;
+    @FindBy(xpath = "//a[contains(@class, 'sf-with-ul')]/..//ul/li/a[contains(@title,'Blouses')]")
+    protected WebElement blousesWomenSubMenu;
+    @FindBy(xpath = "//a[contains(@title,'Dresses') and contains(@class, 'sf-with-ul')]")
+    protected    WebElement dressesOptionMenuBar;
+    @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li[3]/a")
+    protected  WebElement tShirtOptionMenuBar;
     @FindBy (css = "header#header div.header_user_info > a")
     protected WebElement signInBtn;
+    @FindBy(xpath = "/html/head/title")
+    protected WebElement pageTitle;
 
     public CorePage(WebDriver driver) {
         super(driver);
@@ -23,4 +33,23 @@ public abstract class CorePage extends AbsPageFactory{
         return signInBtn;
     }
 
+    public WebElement gettShirtWomenSubMenu() {
+        return tShirtWomenSubMenu;
+    }
+
+    public WebElement getBlousesWomenSubMenu() {
+        return blousesWomenSubMenu;
+    }
+
+    public WebElement getDressesOptionMenuBar() {
+        return dressesOptionMenuBar;
+    }
+
+    public WebElement gettShirtOptionMenuBar() {
+        return tShirtOptionMenuBar;
+    }
+
+    public WebElement getPageTitle(){
+        return pageTitle;
+    }
 }
